4bd5b7298a8e27dd2e645debed06ac05ad08e7c4
dagwor <dagmawiworku11@gmail.com>
2021-05-17 16:33:52 -0500

    lesson 01 app 08

---
+++ lesson01/app08/app08.js
@@ -0,0 +1,4 @@
+const childProcess = require("child_process");
+console.log("Start");
+const newProcess = childProcess.spawn("node", ["computation/_fibonacci.js"], {stdio: "inherit"})
+console.log("End");
