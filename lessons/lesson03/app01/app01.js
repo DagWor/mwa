440801acfbf4e39b4a6b354aff71638338b08d28
Dagmawi <dagmawiworku11@gmail.com>
2021-05-21 10:18:47 -0500

    lesson 03 app 01 complete

---
+++ lessons/lesson03/app01/app01.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const api = require('./api/routes');
+require('./api/data/db');
+require('./api/data/db.connection').opened();
+
+app.set("port", 3000);
+
+app.use('/css', (req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+})
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json({extended: false}));
+app.use('/api', api);
+
+const server = app.listen(app.get("port"), () => {
+    const port = server.address().port;
+    console.log(`Listening on port ${port}`);
+});
