c1a3d36296217caede813dc550faebd62d80e9b7
Dagmawi <dagmawiworku11@gmail.com>
2021-05-21 12:08:35 -0500

    lesson 03 app 06 complete

---
+++ lessons/lesson03/app06/api/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router();
+const controller = require('../controller/games.controller')
+
+router.route('/games')
+.get(controller.gameGetAll)
+.post(controller.gamesAddOne)
+
+
+router.route('/games/:id')
+.get(controller.gameGetById);
+
+router.route('/games/add')
+.post(controller.gamesAddOne)
+
+router.route('/games/:id/publisher')
+.get(controller.gameGetPublisher)
+
+module.exports = router;
